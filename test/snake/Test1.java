a15df6abb92b923e9eb8f992bc3cc8269f0782d2
Thomas Istok <thomas.istok@gmail.com>
2018-11-07 17:58:01 +0100

    Removed tests

--- test/snake/Test1.java
+++ test/snake/Test1.java
@@ -15,24 +15,6 @@ import snake.data.Snake;
  */
 public class Test1 {
 	
-	@Test
-	public void equalGameItemTest1() {
-		//when you create string literals, the objects point to the same value since
-		//they are in constant pool, where as when you use the new operator,
-		//separate objects are created and stored in heap.
-//		String player1 = "player1";
-//		String player2 = new String("player1");
-//		
-//		GameItem item1 = new GameItem(Figure.PLAYER, player1, new Point(1, 2));
-//		GameItem item2 = new GameItem(Figure.PLAYER, player2, new Point(1, 2));
-//		
-//		GameItem item3 = new GameItem(Figure.PLAYER, "player1", new Point(3, 2));
-//		GameItem item4 = new GameItem(Figure.SNAKE, player2, new Point(1, 2));
-//		
-//		assertTrue(item1.equals(item2));
-//		assertFalse(item1.equals(item3));
-//		assertFalse(item1.equals(item4));
-	}
 	
 	@Test
 	public void shiftSnake() {
